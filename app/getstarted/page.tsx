83802db8e702d61a2df29a3a560620ef5c02df4a
itsFiz <hapizzz343@gmail.com>
2024-10-23 18:23:36 +0800

    fix get started

--- app/getstarted/page.tsx
+++ app/getstarted/page.tsx
@@ -17,7 +17,7 @@ export default function GetStartedPage() {
   <div className="relative z-10 flex flex-col gap-20 pt-20 pb-20 px-4 md:px-6">
     <div className="max-w-7xl mx-auto w-full">
      
-    <GetStarted/>
+    <GetStarted />
     </div>
   </div>
 </main>;
